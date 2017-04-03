40cb409de1610769e78044bfc2a691e9f1e019e7
Alvaro Peso <alvaro.peso@csa.es>
2017-04-03 13:29:34 +0200

    Mejorado menú principal, añadido menú secundario e información de rutas

--- js/main.js
+++ js/main.js
@@ -1,12 +1,29 @@
-/* Menú principal */
-$(document).ready(function () {
-    var navmovil = $('nav.movil');
-    var botonAbrirMenu = $('a.menu-abrir');
-    $(navmovil).hide();
-   $(botonAbrirMenu).click(function (event) {
-       event.preventDefault();
-       $('a.menu-abrir i').toggleClass('fa-bars');
-       $('a.menu-abrir i').toggleClass('fa-times');
-      $(navmovil).slideToggle();
-   });
+$(document).ready(function() {
+    menuMovil();
+    $("a.menu-abrir").click(function (event) { //Modificar icono, abrir/cerrar menú móvil
+        event.preventDefault();
+        $('a.menu-abrir i').toggleClass('fa-bars');
+        $('a.menu-abrir i').toggleClass('fa-times');
+        $("nav.movil").slideToggle();
+    });
 });
+
+$( window ).resize(function() {
+    menuMovil();
+});
+
+function menuMovil() {
+    if (window.matchMedia('(max-width: 629px)').matches && $('nav.movil').length == 0)
+    {
+        $("nav.principal").appendTo("body > header").removeClass('principal').addClass('movil');
+        $("nav.movil").hide();
+    }
+
+    if (window.matchMedia('(min-width: 628px)').matches)
+    {
+        $("a.menu-abrir i[class='fa fa-times']").removeClass('fa-times').addClass('fa-bars');
+        $("nav.movil").show();
+        $("nav.movil").removeClass('movil').addClass('principal').appendTo(".barra-menu");
+
+    }
+}
